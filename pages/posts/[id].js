93224432629a24acc83fcd182cacc4cf5a1d9448
Takahiro <tkhr_621@docomo.ne.jp>
2020-11-23 16:59:20 +0900

    マークダウン をレンダー

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,15 +2,22 @@
 
 import { Layout } from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
 
 export default function Post({ postData }) {
+    console.log(postData)
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
@@ -25,7 +32,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData,
