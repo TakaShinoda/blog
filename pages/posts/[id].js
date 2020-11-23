f29a9ad1ec6e5fe92f11b2a8db4170f8a331d9c0
Takahiro <tkhr_621@docomo.ne.jp>
2020-11-23 16:44:50 +0900

    動的ルーディング

---
+++ pages/posts/[id].js
@@ -0,0 +1,34 @@
+// [ で始まり ] で終わるページは、Next.js では動的なページになります。
+
+import { Layout } from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
+
+// idがとりうる値のリストを返す
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData,
+    },
+  }
+}
