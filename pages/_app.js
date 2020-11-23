bf94a7c4ada8c35665d0cb1b7ffae76e5315f44a
Takahiro <tkhr_621@docomo.ne.jp>
2020-11-23 12:40:56 +0900

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-// グローバルなCSSファイルを読み込む
+// この App コンポーネントは、すべてのページに共通するトップレベルのコンポーネントです。
+// たとえば、ページ間を移動するときに状態（state）を保持するために App コンポーネントを使用することができます。
 
+
+// グローバルなCSSファイルを読み込む
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
