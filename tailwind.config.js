7769a624275d73ffebddcb4b185208269cfc8407
Takahiro <tkhr_621@docomo.ne.jp>
2020-12-05 19:01:26 +0900

    PurgeCSS設定修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./components/**/*.jsx', './pages/**/*.jsx'],
+  purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
