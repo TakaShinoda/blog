a99982c646ae52eb66f4afce11b8edb6e7f0fd65
Takahiro <tkhr_621@docomo.ne.jp>
2020-12-06 18:10:27 +0900

    不要なcssファイルのインポートを削除

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -4,7 +4,6 @@ import { Layout } from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import { Date } from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
 import { FaRegCalendarAlt } from 'react-icons/fa'
 
 // getStaticProps、getStaticPaths、getServerSideProps 用の型
