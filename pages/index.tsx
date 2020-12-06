f762633b1519e4eb251172c9a4f311167c61556c
Takahiro <tkhr_621@docomo.ne.jp>
2020-12-07 01:44:08 +0900

    fix styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { Layout, siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
 import { GetStaticProps } from 'next'
 import { FaRegCalendarAlt } from 'react-icons/fa'
+import Image from 'next/image'
 
 export default function Home({
   allPostsData,
@@ -20,22 +21,36 @@ export default function Home({
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className='text-xl pt-1'>
-        <h2 className='text-2xl mt-4 mb-4'>Blog</h2>
-        <ul className='list-none'>
+      <section className="text-xl pt-1">
+        <h2 className="text-2xl mt-4 mb-4">Blog</h2>
+
+        <ul className="list-none">
           {allPostsData.map(({ id, date, title }) => (
-            <li className='mb-5' key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className='text-gray-400'>
-                <span className='mr-1'>
-                <FaRegCalendarAlt />
-                </span>
-                <Date dateString={date} />
-              </small>
-            </li>
+            <div className="inline-flex mr-5 max-w-180">
+              <li
+                className="mb-5 bg-white rounded-lg overflow-hidden shadow-xl p-3"
+                key={id}
+              >
+                <Image
+                src="/images/no_image.png"
+                width='200'
+                height='200'
+                className='w-24 h-24'
+                alt="no-image"
+                />
+                <br/>
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+                <br />
+                <small className="text-gray-400">
+                  <span className="mr-1">
+                    <FaRegCalendarAlt />
+                  </span>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            </div>
           ))}
         </ul>
       </section>
