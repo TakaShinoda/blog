8c150bcf8aac7a98f9f0b7441de2ed42298d83f8
Takahiro <tkhr_621@docomo.ne.jp>
2020-12-12 18:11:50 +0900

    カードコンポーネントのベース

---
+++ components/cardList.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const CardList = (allPostsData) => {
+    console.log(allPostsData)
+    return (
+        <div className="inline-flex mr-5 max-w-180">
+            <ul className="list-none">
+            <li
+                className="mb-5 bg-green-100 rounded-lg overflow-hidden shadow-xl p-3"
+            >
+                aaa
+            </li>
+            </ul>
+            
+        </div>
+    )
+}
