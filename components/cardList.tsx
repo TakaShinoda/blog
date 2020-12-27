0bd5c53109d7e99c0b9ed36b17503b2b154c257a
Takahiro <tkhr_621@docomo.ne.jp>
2020-12-27 13:14:16 +0900

    feat: トップに画像を表示する

--- components/cardList.tsx
+++ components/cardList.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { FaRegCalendarAlt } from 'react-icons/fa'
 import { Date } from '../components/date'
 
-export const CardList = ({allPostsData}) => {
+export const CardList = ({ allPostsData }) => {
   return (
     <>
       <ul className="list-none">
@@ -14,19 +14,29 @@ export const CardList = ({allPostsData}) => {
               className="mb-5 rounded-lg overflow-hidden shadow-xl p-3 w-44 bg-white"
               key={id}
             >
-              <Image
-                src="/images/no_image.png"
-                width="200"
-                height="200"
-                className="w-24 h-24"
-                alt="no-image"
-              />
+              {image ? (
+                <Image
+                  src={image}
+                  width="200"
+                  height="200"
+                  className="w-24 h-24"
+                  alt="thumbnail"
+                />
+              ) : (
+                <Image
+                  src="/images/no_image.png"
+                  width="200"
+                  height="200"
+                  className="w-24 h-24"
+                  alt="no-image"
+                />
+              )}
               <br />
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
               <br />
-              
+
               <small className="text-gray-400">
                 <span className="mr-1">
                   <FaRegCalendarAlt />
